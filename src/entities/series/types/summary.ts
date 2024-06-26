2933afc0cd52fca5ab7f3f41f6aac45615a26168
kuman514 <hadjadj0@gmail.com>
2024-06-26 22:26:05 +0900

    docs: add summary types for entities (character, comic, series, story)

---
+++ src/entities/series/types/summary.ts
@@ -0,0 +1,17 @@
+/**
+ * Referred from: https://developer.marvel.com/docs
+ */
+
+export interface SeriesSummary {
+  /**
+   * @desc
+   * The path to the individual series resource.,
+   */
+  resourceURI: string;
+
+  /**
+   * @desc
+   * The canonical name of the series.
+   */
+  name: string;
+}
